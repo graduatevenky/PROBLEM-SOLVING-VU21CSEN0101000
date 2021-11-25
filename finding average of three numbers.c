25d33a6c382639e62a8ec5e91d213ffa3113e9eb
graduatevenky <93900849+graduatevenky@users.noreply.github.com>
2021-11-25 15:37:52 +0530

    Create finding average of three numbers.c

---
+++ finding average of three numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    float a,b,c,avg;
+    printf("finding of average of three numbers");
+    scanf("%f",&a);
+    scanf("%f",&b);
+    scanf("%f",&c);
+    avg=(a+b+c)/3;
+    printf("%f",avg);
+    
+
+    return 0;
+}
