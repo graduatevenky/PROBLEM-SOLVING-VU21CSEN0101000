008a23baf9770b176628d908da39d9b21d3ea223
graduatevenky <93900849+graduatevenky@users.noreply.github.com>
2021-11-25 15:39:08 +0530

    Create intrest of money.c

---
+++ intrest of money.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main()
+{
+    float p,t,r,si;
+    scanf("%f",&p);
+    scanf("%f",&t);
+    scanf("%f",&r);
+    printf("to know the simple intrest of the money");
+    si=(p*t*r)/100;
+    printf("%f",si);
+    
+
+    return 0;
+}
