d21aed6d48c6abf1704fa277089125d923a9e9b7
graduatevenky <93900849+graduatevenky@users.noreply.github.com>
2021-11-25 15:40:11 +0530

    Create swapping of two numbers.c

---
+++ swapping of two numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    printf("to swap the numbers is an");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    a=b;
+    b=c;
+    c=a;
+    printf("%d %d",a,b);
+
+    return 0;
+}
