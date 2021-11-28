caf91938820dd29ce69b1b8a63b986b8814e8f97
graduatevenky <93900849+graduatevenky@users.noreply.github.com>
2021-11-28 14:27:13 +0530

    Create loops .c

---
+++ loops .c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=10;
+    printf("to find the how many members in the class");
+    for(a=10;a>=0;a--)
+    {
+        printf("%d \t",a);
+    }
+
+    return 0;
+}
+
