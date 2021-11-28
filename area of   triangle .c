e81b3af1b5ce215677714d7a42bfae5c87e27432
graduatevenky <93900849+graduatevenky@users.noreply.github.com>
2021-11-28 15:27:51 +0530

    Create area of   triangle .c

---
+++ area of   triangle .c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    float b,h,a;
+    printf("to find the area of of the triangle");
+    scanf("%f %f",&b,&h);
+    a=(b*h)/2;
+    printf("%f",a);
+    return 0;
+}
