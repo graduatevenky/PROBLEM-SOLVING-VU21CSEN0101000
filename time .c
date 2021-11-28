81e11ede459f5037f1e17bdc713b155f05ef93d7
graduatevenky <93900849+graduatevenky@users.noreply.github.com>
2021-11-28 10:37:20 +0530

    Create time .c

---
+++ time .c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    float d,s,t;
+    printf("to find the time");
+    scanf("%f",&d);
+    scanf("%f",&s);
+    t=d/s;
+    printf("%f",d);
+
+    return 0;
+}
