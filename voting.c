61d2d6f6f580ed208930aec24bfe3a12af0937dd
graduatevenky <93900849+graduatevenky@users.noreply.github.com>
2021-11-28 10:39:11 +0530

    Create voting.c

---
+++ voting.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    printf("to find the member for voting eligiable are not");
+    scanf("%d",&x);
+    if(x>18)
+    {
+        printf("you can eligiable for voting");
+    }
+    else
+    {
+        printf("you are not eligiable for voting");
+    }
+
+    return 0;
+}
