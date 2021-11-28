7f3e76c773259f47676fbd4a0f55cf213decf4e4
graduatevenky <93900849+graduatevenky@users.noreply.github.com>
2021-11-28 14:25:04 +0530

    Create negative-to-positive .c

---
+++ negative-to-positive .c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=10;
+    printf("findi ng the number negative to pos");
+    for(a=10;a>=-10;a--)
+    {
+        printf("%d",a);
+    }
+
+    return 0;
+}
