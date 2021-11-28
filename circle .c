a9aebe5f2df33ed65be755855b946f3c89e99f28
graduatevenky <93900849+graduatevenky@users.noreply.github.com>
2021-11-28 14:44:59 +0530

    Create circle .c

---
+++ circle .c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    float r,c,d,a,l,m;
+    printf("to find the circumference and area of the circle");
+    scanf("%f",&a);
+    scanf("%f",&l);
+    scanf("%f",&m);
+    a=180*r*r;
+    l=2*r;
+    m=c/(2*180);
+    printf("%f %f %f",a,l,m);
+    return 0;
+}
+
