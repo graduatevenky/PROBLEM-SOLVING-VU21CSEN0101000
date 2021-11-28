540d450303ebc38417b1714f279f1bce42f90199
graduatevenky <93900849+graduatevenky@users.noreply.github.com>
2021-11-28 14:46:17 +0530

    Create area of equi  triangle .c

---
+++ area of equi  triangle .c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    float a,b;
+    printf("to find the area of the triangle");
+    scanf("%f",&a);
+    b=sqrt3*(a*a);
+    printf("%f",b);
+
+    return 0;
+}
