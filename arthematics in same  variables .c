d9e002b96f1fc7e75243a636b48a1ce4cb7de300
graduatevenky <93900849+graduatevenky@users.noreply.github.com>
2021-11-28 10:55:12 +0530

    Create arthematics in same  variables .c

---
+++ arthematics in same  variables .c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c,d,e;
+    float g;
+    printf("to perform all arthematics in same variables");
+    scanf("%d %d",&a,&b);
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    g=a/b;
+    printf("%d %d %d \n",c,d,e);
+    printf("%f \n",g);
+    return 0;
+}
